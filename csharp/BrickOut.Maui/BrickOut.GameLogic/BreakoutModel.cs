992c9026682d319dc9ca258e973616c72a46420c
Patrick Burrows <pburrows@burrowscorp.com>
2023-10-29 05:54:29 -0400

    After refactoring the DisplayItem Rectangle into a separate Size and Point

---
+++ csharp/BrickOut.Maui/BrickOut.GameLogic/BreakoutModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Drawing;
+
+namespace BrickOut.GameLogic;
+
+public class GameBoard
+{
+    public int Height { get; set; } = 640;
+    public int Width { get; set; } = 480;
+
+    public List<Brick> Bricks { get; set; } = new();
+    public Ball Ball { get; set; }
+    public Paddle Paddle { get; set; }
+
+    public static GameBoard NewGame()
+    {
+        var board = new GameBoard();
+
+        board.Paddle = new Paddle(0,635);
+        board.Ball = new Ball(20, 630);
+        
+        // create bricks
+        for (var row = 0; row < 6; row++)
+        {
+            int y = row * 10;
+            // 24 columns == 480 width / 20 pixel wide bricks
+            for (var col = 0; col < 24; col++)
+            {
+                int x = col * 20;
+                board.Bricks.Add(new Brick(x, y));
+            }
+        }
+
+        return board;
+    }
+}
+
+
+public interface DisplayItem
+{
+    public Size Shape { get; set; }
+    public Point Location { get; set; }
+}
+
+public class Brick : DisplayItem
+{
+    public Size Shape { get; set; }
+    public Point Location { get; set; }
+
+    public Brick(int x, int y)
+    {
+        Shape = new Size(20, 10);
+        Location = new Point(x, y);
+    }
+    
+}
+
+public class Ball : DisplayItem
+{
+    public Size Shape { get; set; }
+    public Point Location { get; set; }
+
+    public Ball(int x, int y)
+    {
+        Shape = new Size(5, 5);
+        Location = new Point(x, y);
+    }
+}
+
+public class Paddle : DisplayItem
+{
+    public Size Shape { get; set; }
+    public Point Location { get; set; }
+
+    public Paddle(int x, int y)
+    {
+        Shape = new Size(40, 5);
+        Location = new Point(x, y);
+    }
+}
+
+public class BrickOutGame {}
---
+++ csharp/BrickOut.Maui/BrickOut.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using BrickOut.GameLogic;
+using Color = System.Windows.Media.Color;
+using ColorConverter = System.Windows.Media.ColorConverter;
+using Rectangle = System.Windows.Shapes.Rectangle;
+
+namespace BrickOut.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public GameBoard CurrentGame { get; set; } = GameBoard.NewGame();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            GameCanvas.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F5E2E0"));
+            GameCanvas.MouseMove += GameCanvasOnMouseMove;
+            DrawGameBoard();
+        }
+
+        private void GameCanvasOnMouseMove(object sender, MouseEventArgs e)
+        {
+            var position = e.GetPosition(GameCanvas);
+            // CurrentGame.Paddle.Shape  // .X = position.X;
+        }
+
+        private List<Color> BrickColors = new()
+        {
+            (Color)ColorConverter.ConvertFromString("#BC4A3C"),
+            (Color)ColorConverter.ConvertFromString("#CE7064"),
+            (Color)ColorConverter.ConvertFromString("#D88D83"),
+            (Color)ColorConverter.ConvertFromString("#E2A9A2"),
+            (Color)ColorConverter.ConvertFromString("#AB4336"),
+            (Color)ColorConverter.ConvertFromString("#8C372C"),
+            (Color)ColorConverter.ConvertFromString("#6D2B22"),
+        };
+
+
+        private void DrawGameBoard()
+        {
+            GameCanvas.Children.Clear();
+
+            foreach (var brick in CurrentGame.Bricks)
+            {
+                AddRectangleToCanvas(
+                    GetNextBrickColorBrush(),
+                    brick.Location.X,
+                    brick.Location.Y,
+                    brick.Shape.Width,
+                    brick.Shape.Height);
+            }
+
+            AddRectangleToCanvas(
+                new SolidColorBrush(Colors.Red),
+                CurrentGame.Paddle.Location.X,
+                CurrentGame.Paddle.Location.Y,
+                CurrentGame.Paddle.Shape.Width,
+                CurrentGame.Paddle.Shape.Height);
+            
+             AddRectangleToCanvas(
+                            new SolidColorBrush(Colors.Green),
+                            CurrentGame.Ball.Location.X,
+                            CurrentGame.Ball.Location.Y,
+                            CurrentGame.Ball.Shape.Width,
+                            CurrentGame.Ball.Shape.Height);
+        }
+
+        private void AddRectangleToCanvas(SolidColorBrush brush, int x, int y, int width, int height)
+        {
+            var wpfRect = new Rectangle();
+            wpfRect.Fill = brush;
+            wpfRect.Width = width;
+            wpfRect.Height = height;
+            GameCanvas.Children.Add(wpfRect);
+            Canvas.SetTop(wpfRect, y);
+            Canvas.SetLeft(wpfRect, x);
+        }
+
+        private int nextBrickColorIndex = 0;
+        private SolidColorBrush GetNextBrickColorBrush()
+        {
+            var brush = new SolidColorBrush(BrickColors[nextBrickColorIndex]);
+            nextBrickColorIndex += 1;
+            if (nextBrickColorIndex >= BrickColors.Count)
+            {
+                nextBrickColorIndex = 0;
+            }
+
+            return brush;
+        }
+    }
+}
